d2a053c9cb8e67596df8b18f4d5d7f85ffd313b4
Vinibcandido <vinicius.candido007@gmail.com>
2023-07-12 21:52:20 -0300

    adição de utilização dos numeros e tecla de clear

---
+++ script.js
@@ -0,0 +1,57 @@
+//Variaveis globais para transporte de dados
+let _conteudoLblVisor = []
+let _conteudoVisor = []
+
+//Visor da calculadora
+const lblresultadoVisor = document.getElementById("lblresultadoVisor")
+const resultadoVisor = document.getElementById("resultadoVisor")
+
+//Botões auxiliares e suas funcionalidades
+const limpeza = document.getElementById("limpeza")
+limpeza.addEventListener("click", LimparVisor)
+
+const maisMenos = document.getElementById("maisMenos")
+
+const cento = document.getElementById("cento")
+
+//Botões operadores e suas funcionalidades
+const divisao = document.getElementById("divisao")
+
+const multiplicacao = document.getElementById("multiplicacao")
+
+const subtracao = document.getElementById("subtracao")
+
+const adicao = document.getElementById("adicao")
+
+const resultado = document.getElementById("resultado")
+
+//Botões de números e suas funcionalidades
+let botoes = document.getElementsByClassName("numeros")
+
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].addEventListener("click", function () {
+    AdicionaNumeroVisor(this.textContent)
+    limpeza.textContent = "C"
+  })
+}
+
+//funções da calculadora
+function MostrarResultado(resultado) {
+  //apresenta o resultado no visor
+}
+
+function AlteraLblVisor(operacao) {
+  //apresenta a conta respectiva ao resultado
+}
+
+function LimparVisor() {
+  _conteudoLblVisor = []
+  _conteudoVisor = []
+  lblresultadoVisor.textContent = ""
+  resultadoVisor.value = ""
+  limpeza.textContent = "AC"
+}
+
+function AdicionaNumeroVisor(numero) {
+  resultadoVisor.value += numero
+}
