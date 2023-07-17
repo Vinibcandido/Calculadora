cec4baca8c80b8666f5e00cc12b18577b0b96b03
Vinibcandido <vinicius.candido007@gmail.com>
2023-07-17 20:49:21 -0300

    finalização do script dos botões operadores

--- script.js
+++ script.js
@@ -1,57 +1,147 @@
 //Variaveis globais para transporte de dados
 let _conteudoLblVisor = []
-let _conteudoVisor = []
+let _operadorSelecionado = false
 
 //Visor da calculadora
 const lblresultadoVisor = document.getElementById("lblresultadoVisor")
 const resultadoVisor = document.getElementById("resultadoVisor")
+resultadoVisor.addEventListener("keypress", function (e) {
+  // Permitir apenas números e o ponto/virgula
+  var charCode = e.which ?? e.keyCode
+  var tecla = String.fromCharCode(charCode)
+  var valorAtual = e.target.value
 
-//Botões auxiliares e suas funcionalidades
-const limpeza = document.getElementById("limpeza")
-limpeza.addEventListener("click", LimparVisor)
+  if (
+    charCode > 31 &&
+    (charCode < 48 || charCode > 57) &&
+    charCode != 46 &&
+    charCode != 44
+  ) {
+    e.preventDefault()
+  } else if (
+    (tecla == "," || tecla == ".") &&
+    (valorAtual.includes(",") || valorAtual.includes("."))
+  ) {
+    // Impedir mais de uma vírgula ou ponto
+    e.preventDefault()
+  } else if ((tecla == "," || tecla == ".") && valorAtual.length == 0) {
+    // Impedir vírgula ou ponto no início
+    e.preventDefault()
+  } else {
+    return true
+  }
+})
 
-const maisMenos = document.getElementById("maisMenos")
-
-const cento = document.getElementById("cento")
-
-//Botões operadores e suas funcionalidades
-const divisao = document.getElementById("divisao")
-
-const multiplicacao = document.getElementById("multiplicacao")
-
-const subtracao = document.getElementById("subtracao")
-
-const adicao = document.getElementById("adicao")
-
-const resultado = document.getElementById("resultado")
-
-//Botões de números e suas funcionalidades
-let botoes = document.getElementsByClassName("numeros")
-
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].addEventListener("click", function () {
-    AdicionaNumeroVisor(this.textContent)
-    limpeza.textContent = "C"
-  })
-}
-
-//funções da calculadora
-function MostrarResultado(resultado) {
-  //apresenta o resultado no visor
-}
-
-function AlteraLblVisor(operacao) {
-  //apresenta a conta respectiva ao resultado
+function AdicionarNumeroAoVisor(numero) {
+  if (_operadorSelecionado) {
+    _operadorSelecionado = false
+    resultadoVisor.value = ""
+  }
+  let valorAtual = resultadoVisor.value
+  if (numero == "," && (valorAtual.includes(",") || valorAtual.includes("."))) {
+    return true
+  } else if (numero == "," && valorAtual.length == 0) {
+    return true
+  } else {
+    resultadoVisor.value += numero
+  }
 }
 
 function LimparVisor() {
-  _conteudoLblVisor = []
-  _conteudoVisor = []
   lblresultadoVisor.textContent = ""
   resultadoVisor.value = ""
-  limpeza.textContent = "AC"
+  _conteudoLblVisor = []
 }
 
-function AdicionaNumeroVisor(numero) {
-  resultadoVisor.value += numero
+function AtualizarLblVisor() {
+  lblresultadoVisor.textContent = ""
+  for (let n = 0; n < _conteudoLblVisor.length; n++) {
+    lblresultadoVisor.textContent += _conteudoLblVisor[n]
+  }
+}
+
+function ExecutarAcaoDeOperador(operador) {
+  if (resultadoVisor.value != 0) {
+    if (_conteudoLblVisor.length == 2 && operador != "=") {
+      let resultado = EfetuarOperacao(operador)
+      LimparVisor()
+      _conteudoLblVisor.push(resultado)
+      _conteudoLblVisor.push(operador)
+      resultadoVisor.value = resultado
+      _operadorSelecionado = true
+    } else if (_conteudoLblVisor.length == 2 && operador == "=") {
+      let resultado = EfetuarOperacao(_conteudoLblVisor[1])
+      _conteudoLblVisor.push(resultadoVisor.value)
+      resultadoVisor.value = resultado
+      _operadorSelecionado = true
+    } else if (_conteudoLblVisor.length == 3 && operador != "=") {
+      _conteudoLblVisor = []
+      _conteudoLblVisor.push(resultadoVisor.value)
+      _conteudoLblVisor.push(operador)
+      _operadorSelecionado = true
+    } else {
+      _conteudoLblVisor.push(resultadoVisor.value)
+      _conteudoLblVisor.push(operador)
+      _operadorSelecionado = true
+    }
+    AtualizarLblVisor()
+  }
+}
+
+function EfetuarOperacao(operador) {
+  let resultado
+  switch (operador) {
+    case "+":
+      resultado = Somar()
+      break
+
+    case "-":
+      resultado = Subtrair()
+      break
+
+    case "÷":
+      resultado = Dividir()
+      break
+
+    case "×":
+      resultado = Multiplicar()
+      break
+  }
+  return substituirPontoPorVirgula(resultado)
+}
+
+function substituirVirgulaPorPonto(str) {
+  return String(str).replace(/,/g, ".")
+}
+
+function substituirPontoPorVirgula(str) {
+  return String(str).replace(".", ",")
+}
+
+function Somar() {
+  return (
+    Number(substituirVirgulaPorPonto(_conteudoLblVisor[0])) +
+    Number(substituirVirgulaPorPonto(resultadoVisor.value))
+  )
+}
+
+function Subtrair() {
+  return (
+    Number(substituirVirgulaPorPonto(_conteudoLblVisor[0])) -
+    Number(substituirVirgulaPorPonto(resultadoVisor.value))
+  )
+}
+
+function Dividir() {
+  return (
+    Number(substituirVirgulaPorPonto(_conteudoLblVisor[0])) /
+    Number(substituirVirgulaPorPonto(resultadoVisor.value))
+  )
+}
+
+function Multiplicar() {
+  return (
+    Number(substituirVirgulaPorPonto(_conteudoLblVisor[0])) *
+    Number(substituirVirgulaPorPonto(resultadoVisor.value))
+  )
 }
